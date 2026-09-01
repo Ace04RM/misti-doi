459dc5a3db6be1ef2c80f84d82d2d75c0e4b43a7
Keshav Dey <keshavdey10@gmail.com>
2026-09-01 16:51:59 +0530

    Further optimize animation performance

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const counterObserver = new IntersectionObserver((entries) => {
 
 counters.forEach(c => counterObserver.observe(c));
 // Floating background particles
-const particleCount = 10;
+const particleCount = 6;
 for (let i = 0; i < particleCount; i++) {
     const p = document.createElement("div");
     p.className = "particle";
